e19566af5b37321a4c91423f5d094f075f56bb52
Daniel <tawata.daniel@gmail.com>
2023-06-22 15:45:38 -0700

    finished css

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import styles from '../styles/Home.module.css'
+import { useState, useRef, useEffect } from 'react';
 
 export default function Home() {
+  const [css, setCss] = useState(`${styles.container}`);
+
+  const changeCss = () => {
+    if (css === `${styles.container}`) setCss(`${styles.container} ${styles.active}`);
+    else setCss(`${styles.container}`);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.one}></div>
-      <div className={styles.two}></div>
-      <div className={styles.three}>
+    <div className={css}>
+      <div className={styles.one}>one</div>
+      <div className={styles.two}>two</div>
+      <div onClick={changeCss} className={styles.three}>
         <div className={styles.three_top}>Top</div>
         <div className={styles.three_mid}>Mid</div>
         <div className={styles.three_bot}>Bottom</div>
